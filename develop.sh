b08a99c82c92ade047c107970ab32da4112545a1
Zachary McCord <zjmccord@gmail.com>
2018-05-31 14:08:48 -0400

    let's just use pytest

--- develop.sh
+++ develop.sh
@@ -2,7 +2,6 @@
 export DJANGO_SETTINGS_MODULE=config.settings.local
 repo_dir="$(cd $(dirname "$BASH_SOURCE"); pwd)"
 . "$repo_dir/venv/bin/activate"
-apps_dir="$repo_dir/h5i5"
-export PYTHONPATH="$apps_dir:$PYTHONPATH"
+export PYTHONPATH="$repo_dir:$PYTHONPATH"
 alias da=django-admin
 alias dj=django-admin
